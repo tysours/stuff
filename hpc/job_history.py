d6331be71e7927cfd0943417eedc04afed754c0a
Ty Sours <tsours@ucdavis.edu>
2022-12-14 17:43:52 -0800

    Rework job counting and add cache

--- hpc/job_history.py
+++ hpc/job_history.py
@@ -4,6 +4,8 @@ import os
 import sys
 import subprocess
 import calendar
+import json
+import numpy as np
 from datetime import date, timedelta
 
 
@@ -21,7 +23,7 @@ class JobHistory:
         end_month (int or None): Year to use for end of search (today if None
             and end_year=None, else December (31) if end_year!=None).
     """
-    def __init__(start_year, start_month, end_year=None, end_month=None):
+    def __init__(self, start_year, start_month, end_year=None, end_month=None):
         self.start = date(start_year, start_month, 1)
         if end_year is None and end_month is None:
             self.end = date.today()
@@ -36,65 +38,79 @@ class JobHistory:
         self.elapsed_time = 0
         self.cpu_time = 0
 
-    def get_month_of_jobs(self, year=None, month=None):
-        end_date = start_date + last_day(year, month)
-        #fmt = '--format="user,jobid%15,state%20,CPUTimeRaw,ElapsedRaw"'
-        fmt_arg = '--format="user,jobid%15,ElapsedRaw,CPUTimeRaw"'
-        date_args = f"--starttime={self.start} --endtime={end_date}"
+    def get_month_of_jobs(self, year=None, month=None, verbose=False):
+        year = year if year else self.start.year
+        month = month if month else self.start.month
+        start_date = date(year, month, 1)
+        end_date = self.last_day(year, month)
+        if verbose:
+            print(f"Checking jobs from {start_date} to {end_date}")
+        fmt_arg = "--format='user,jobid%15,ElapsedRaw,CPUTimeRaw'"
+        date_args = f"--starttime={start_date} --endtime={end_date}"
 
-        command = f"sacct {date_args} {fmt_arg} | grep {user}"
-        jobs = subprocess.check_output(command.split())
-        elapsed, cpu = list(map(float, jobs.split()[-2:]))
-        n_jobs = len(cpu)
+        command = f"sacct {date_args} {fmt_arg} | grep {self.user} > .jobhistory.temp"
+        os.system(command)
+        jobs = np.loadtxt(".jobhistory.temp", dtype=int, usecols=(2,3))
+        os.remove(".jobhistory.temp")
+
+        nonzero = [] # filter out cancelled jobs w/ 0 run time
+        for _id, row in enumerate(jobs):
+            if row.sum() != 0:
+                nonzero.append(_id)
+        jobs = jobs[nonzero]
+
+        n_jobs = jobs.shape[0]
+        if n_jobs:
+            elapsed, cpu = np.sum(jobs, axis=0) / 3600.0
+        else:
+            elapsed, cpu = 0.0, 0.0
+        self.cache = n_jobs, elapsed, cpu
         return n_jobs, elapsed, cpu
 
     def last_day(self, year=None, month=None):
         year = year if year else self.start.year
         month = month if month else self.start.month
-        return date(year, month, calendar.monthrange(year, month))
+        n_days = calendar.monthrange(year, month)[1]
+        return date(year, month, n_days)
 
     def update_total(self):
-        n_jobs, elapsed, cpu = self.get_month_of_jobs()
+        if self.cache:
+            print(self.start)
+            n_jobs, elapsed, cpu = self.cache
+        else:
+            n_jobs, elapsed, cpu = self.get_month_of_jobs(verbose=True)
         self.jobs += n_jobs
         self.elapsed_time += elapsed
         self.cpu_time += cpu
 
     def next_month(self, year=None, month=None):
-        return self.last_day(year, month)
+        return self.last_day(year, month) + timedelta(days=1)
 
     def get_history(self):
         while self.start < self.end:
             self.update_total()
-            self.start = self.last_day() + timedelta(days=1)
+            self.start = self.next_month()
 
-def get_month_of_jobs(start_date):
-    end_date = last_day(start_date.year, start_date.month)
-    print(end_date)
+    @property
+    def cache(self):
+        if not hasattr("self", "_cache"):
+            if "history.json" in os.listdir():
+                with open("history.json") as file:
+                    self._cache = json.loads(file.read())
+            else:
+                self._cache = {}
+        return self._cache.get(str(self.start))
 
-    fmt_arg = '--format="user,jobid%15,state%20,CPUTimeRaw,ElapsedRaw'
-    date_args = f"--starttime={start_date} --endtime={end_date}"
-    user = os.environ["USER"]
+    @cache.setter
+    def cache(self, vals):
+        self._cache[str(self.start)] = vals
+        with open("history.json", "w") as file:
+            file.write(json.dumps(self._cache, indent=2))
 
-    command = f"sacct {date_args} {fmt_arg} | grep {user}"
-    #jobs = subprocess.check_output(command.split())
-    print(end_date + timedelta(days=1))
-    print()
+if __name__ == "__main__":
+    start_year = 2019
+    start_month = 1
 
-
-def last_day(year, month):
-    n_days = calendar.monthrange(year, month)[1]
-    return date(year, month, n_days)
-
-start_year = 2019
-start_month = 1
-
-start = date(start_year, start_month, 1)
-today = date.today()
-
-next_month = start + timedelta(days=30)
-
-while next_month < today:
-    print(next_month)
-    get_month_of_jobs(next_month)
-    next_month += timedelta(days=31)
-
+    history = JobHistory(start_year, start_month)
+    history.get_history()
+    print(history.jobs, history.elapsed_time, history.cpu_time)
